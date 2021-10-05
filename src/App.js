9590d8a2bd44e7c23ddeed91fbd9eb931d8e04be
muradzalov <mzalov@gmail.com>
2021-10-04 20:43:53 -0500

    Pushing the latest additions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   {
@@ -17,11 +19,26 @@ const items = [
   }
 ]
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+]
+
 const App = () => {
+
   return (
     <div>
-      <Search />
-      <Accordion items={items} />
+      <Translate />
     </div>
   )
 }
